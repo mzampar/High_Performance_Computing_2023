2898d106772f18724407ba2540656c04795783b2
mzampar <marco.zampar00@gmail.com>
2024-03-15 13:00:12 +0100

    test shell

--- Excercise_2/Ex3/mandelbrot_omp_scaling.sh
+++ Excercise_2/Ex3/mandelbrot_omp_scaling.sh
@@ -19,7 +19,7 @@ mpicc -o ./build/mandelbrot mandelbrot.c -lm -fopenmp
 out_csv="./results/mandelbrot_mpi_execution_times.csv"
 
 # Number of repetitions
-repetitions=100
+repetitions=10
 
 # OMP scaling
 echo "OMP scaling:"
