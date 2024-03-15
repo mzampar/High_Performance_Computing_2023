c245bca4f92ce406443473159502f076f1168e77
mzampar <marco.zampar00@gmail.com>
2024-03-15 11:55:29 +0100

    test shell

--- Excercise_2/Ex3/mandelbrot_mpi_scaling.sh
+++ Excercise_2/Ex3/mandelbrot_mpi_scaling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=4 
-#SBATCH --ntasks-per-node=6    
+#SBATCH --ntasks-per-node=12  
 #SBATCH --time=02:00:00
 #SBATCH --partition=THIN
 #SBATCH --job-name=HPC_ex02_mandelbrot
