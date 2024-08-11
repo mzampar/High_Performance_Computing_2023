d4121997800b1a4161c6cb5d5234104e8a73faff
mzampar <marco.zampar00@gmail.com>
2024-08-11 23:42:28 +0200

    scaling

--- Excercise_2_c/mandelbrot.c
+++ Excercise_2_c/mandelbrot.c
@@ -133,15 +133,15 @@ int main(int argc, char *argv[]) {
             MPI_Comm_rank(sub_comm, &sub_rank);
             MPI_Comm_size(sub_comm, &sub_size);
             // Gather data among processes in this sub-communicator
-            if (sub_size >1) {
+            if (sub_size > 1) {
                 printf("Rank %d in sub_comm %d\n", rank, sub_rank);
                 MPI_Gather(local_matrix + my_rows * nx, nx, MPI_CHAR,
                         gathered_matrix + my_rows * size * nx, nx, MPI_CHAR,
                         0, sub_comm);
-            } else {
-               printf("Rank %d in sub_comm %d\n", rank, sub_rank);
-               memcpy(gathered_matrix + my_rows * size * nx, local_matrix + my_rows * nx, nx * sizeof(char));
-            }
+            } //else {
+               //printf("Rank %d in sub_comm %d\n", rank, sub_rank);
+              // memcpy(gathered_matrix + my_rows * size * nx, local_matrix + my_rows * nx, nx * sizeof(char));
+            //}
 
             MPI_Comm_free(&sub_comm);
         }
