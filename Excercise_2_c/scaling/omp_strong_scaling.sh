51398c7b4e9bb2562008d58d01d2b63f332baf18
mzampar <marco.zampar00@gmail.com>
2024-08-11 11:10:02 +0200

    scaling

--- Excercise_2_c/scaling/omp_strong_scaling.sh
+++ Excercise_2_c/scaling/omp_strong_scaling.sh
@@ -36,7 +36,7 @@ for ((i=1; i<=$repetitions; i++)); do
 
         echo "Running repetition $i with $threads OMP threads..."
         export OMP_NUM_THREADS=$threads
-        elapsed_time=$(mpirun -np 1 --map-by socket --bind-to socket ./build/mandelbrot 800 1000 -1.5 -1.25 0.5 1.25 255 | grep "Elapsed time:" | awk '{print $3}')
+        elapsed_time=$(mpirun -np 1 --map-by socket --bind-to socket ./build/mandelbrot 10000 10000 -1.5 -1.25 0.5 1.25 255 | grep "Elapsed time:" | awk '{print $3}')
 
         echo "$i,$threads,$elapsed_time" >> "$out_csv"
     done
