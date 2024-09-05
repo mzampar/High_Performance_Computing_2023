ec01d6d54301138d8adf7352d43701c38e008f64
mzampar <marco.zampar00@gmail.com>
2024-09-05 12:41:34 +0200

    rebase

---
+++ Excercise_1/compile_benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=00:30:00
+#SBATCH --partition=THIN
+#SBATCH --exclusive
+
+module load openMPI/4.1.5/gnu
+cd osu-micro-benchmarks-7.3/
+
+./configure CC=/opt/programs/openMPI/4.1.6/bin/mpicc CXX=/opt/programs/openMPI/4.1.6/bin/mpicxx
+make
+make install
