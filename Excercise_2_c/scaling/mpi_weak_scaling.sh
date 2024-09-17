f89e6b3abae72ae8a6e6498af83ef1b5e36d4c1c
mzampar <marco.zampar00@gmail.com>
2024-09-17 16:25:47 +0200

    scaling

--- Excercise_2_c/scaling/mpi_weak_scaling.sh
+++ Excercise_2_c/scaling/mpi_weak_scaling.sh
@@ -30,8 +30,8 @@ export OMP_NUM_THREADS=1
 # Constant amout of work per worker: C = problem size / number of workers
 # Therefore, problem size = C * number of workers 
 
-BASE_ROWS=2500
-BASE_COLS=2500
+BASE_ROWS=3000
+BASE_COLS=3000
 
 lst1=(1 2 4 8)
 lst2=({16..256..8})
