c0e22aa3a0289cd015db3f957c0ea279ea2a7738
mzampar <marco.zampar00@gmail.com>
2024-09-18 14:55:10 +0200

    hybrid

--- Excercise_2_c/scaling/hybrid_strong_scaling.sh
+++ Excercise_2_c/scaling/hybrid_strong_scaling.sh
@@ -19,9 +19,6 @@ mpicc -O3 -march=native -o ./build/mandelbrot mandelbrot.c -lm -fopenmp
 # Output file for storing results
 out_csv="./scaling/results/hybrid_strong_scaling.csv"
 
-# Number of repetitions
-repetitions=1
-
 echo "Iteration,Total Tasks,Total Threads,Elapsed Time(s),Computation Time(s),Gathering Time(s)" > "$out_csv"  # Clear and set header
 
 # Number of OpenMP threads
@@ -39,19 +36,19 @@ threads_list=(1 2 4 8 16)
 
 echo "Running MPI weak scaling."
 
-for ((i=1; i<=$repetitions; i++)); do
-    for total_tasks in "${tasks_list[@]}"; do
-        for total_threads in "${threads_list[@]}"; do
-            export OMP_NUM_THREADS=total_threads
-            export OMP_PLACES=cores
-            export OMP_PROC_BIND=close
-            echo "Running iteration $i with $total_tasks MPI tasks and $total_threads threads."
-            output=$(mpirun -np $total_tasks --map-by numa --bind-to numa ./build/mandelbrot 25000 25000 -1.5 -1.25 0.5 1.25 255)
-            elapsed_time=$(echo "$output" | grep "Elapsed time:" | awk '{print $3}')
-            computation_time=$(echo "$output" | grep "Computation time:" | awk '{print $3}')
-            gathering_time=$(echo "$output" | grep "Gathering time:" | awk '{print $3}')        
-            echo "$i,$total_tasks,$total_threads,$elapsed_time,$computation_time,$gathering_time" >> "$out_csv"
+for total_tasks in "${tasks_list[@]}"; do
+    for total_threads in "${threads_list[@]}"; do
+        export OMP_NUM_THREADS=total_threads
+        export OMP_PLACES=cores
+        export OMP_PROC_BIND=close
+        echo "Running iteration $i with $total_tasks MPI tasks and $total_threads threads."
+        output=$(mpirun -np $total_tasks --map-by numa --bind-to numa ./build/mandelbrot 25000 25000 -1.5 -1.25 0.5 1.25 255)
+        elapsed_time=$(echo "$output" | grep "Elapsed time:" | awk '{print $3}')
+        computation_time=$(echo "$output" | grep "Computation time:" | awk '{print $3}')
+        gathering_time=$(echo "$output" | grep "Gathering time:" | awk '{print $3}')        
+        echo "$i,$total_tasks,$total_threads,$elapsed_time,$computation_time,$gathering_time" >> "$out_csv"
     done
 done
+
 
 echo "Execution completed. Results saved to $out_csv"
